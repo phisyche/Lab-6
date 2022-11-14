c486600ca41b081f26c04f486993f23e0bb76ccc
phisyche <orpheuscrypt@gmail.com>
2022-11-14 19:04:44 +0300

    pop up

--- script.js
+++ script.js
@@ -50,6 +50,29 @@ const map = new mapboxgl.Map({
               'line-opacity': .6
           }
         });
+        map.on('click', 'trails-layer', (e) => {
+            const trail_name = e.features[0].properties.TRLNAME;
+            const property_name = e.features[0].properties.UNITNAME;
+
+            while (Math.abs(e.lngLat.lng - trail_name[0]) > 180) {
+            trail_name[0] += e.lngLat.lng > trail_name[0] ? 360 : -360;
+            }
+             
+            new mapboxgl.Popup()
+            .setLngLat(trail_name)
+            .setHTML(property_name)
+            .addTo(map);
+            });
+             
+            // Cursor changes to a pointer when the mouse is over the trails layer.
+            map.on('mouseenter', 'trails-layer', () => {
+            map.getCanvas().style.cursor = 'pointer';
+            });
+             
+            // Change it back to a pointer when it leaves.
+            map.on('mouseleave', 'trail-layer', () => {
+            map.getCanvas().style.cursor = '';
+            });
     });
 
     
@@ -81,3 +104,41 @@ const map = new mapboxgl.Map({
         visualizePitch: true
     });
     map.addControl(navControl, 'top-right');
+
+
+
+
+
+
+    // /// popus
+
+     
+    // // When a click event occurs on a feature in the places layer, open a popup at the
+    // // location of the feature, with description HTML from its properties.
+    // map.on('click', 'trails-layer', (e) => {
+    // // Copy coordinates array.
+    // const coordinates = e.features[0].geometry.coordinates.slice();
+    // const description = e.features[0].properties.description;
+     
+    // // Ensure that if the map is zoomed out such that multiple
+    // // copies of the feature are visible, the popup appears
+    // // over the copy being pointed to.
+    // while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+    // coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    // }
+     
+    // new mapboxgl.Popup()
+    // .setLngLat(coordinates)
+    // .setHTML(description)
+    // .addTo(map);
+    // });
+     
+    // // Change the cursor to a pointer when the mouse is over the places layer.
+    // map.on('mouseenter', 'trails-layer', () => {
+    // map.getCanvas().style.cursor = 'pointer';
+    // });
+     
+    // // Change it back to a pointer when it leaves.
+    // map.on('mouseleave', 'places', () => {
+    // map.getCanvas().style.cursor = '';
+    // });
